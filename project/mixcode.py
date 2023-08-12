61186d8101a9115e5670fcc86f40a1ca12623f3a
firss17 <fakhrifirdaus978@gmail.com>
2023-08-12 10:28:59 +0700

    code mix to ubidots

---
+++ project/mixcode.py
@@ -0,0 +1,99 @@
+import time
+import json
+import serial
+import modbus_tk.defines as cst
+from modbus_tk import modbus_rtu
+import RPi.GPIO as GPIO
+
+channelkipas = 23
+channellampu = 24
+
+# GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channelkipas, GPIO.OUT)
+GPIO.setup(channellampu, GPIO.OUT)
+
+def motor_on(pin):
+    GPIO.output(pin, GPIO.HIGH)  # Turn motor on
+
+
+def motor_off(pin):
+    GPIO.output(pin, GPIO.LOW)  # Turn motor off
+
+
+if __name__ == "__main__":
+    try:
+        # Connect to the slave
+        serial1 = serial.Serial(
+                               port='/dev/ttyS0',
+                               baudrate=9600,
+                               bytesize=8,
+                               parity='N',
+                               stopbits=1,
+                               xonxoff=0
+                              )
+
+        serial2 = serial.Serial(
+                               port='/dev/ttyS0',
+                               baudrate=9600,
+                               bytesize=8,
+                               parity='N',
+                               stopbits=1,
+                               xonxoff=0
+                              )
+
+        master1 = modbus_rtu.RtuMaster(serial1)
+
+        master2 = modbus_rtu.RtuMaster(serial2)
+        master1.set_timeout(2.0)
+        master1.set_verbose(True)
+        master2.set_timeout(2.0)
+        master2.set_verbose(True)
+        # Changing power alarm value to 100 W
+        # master.execute(1, cst.WRITE_SINGLE_REGISTER, 1, output_value=100)
+        dict_payload = dict()
+
+        while True:
+            data1 = master1.execute(1, cst.READ_INPUT_REGISTERS, 0, 10)
+            data2 = master2.execute(1, cst.READ_INPUT_REGISTERS, 0, 10)
+            motor_on(channelkipas)
+            time.sleep(1)
+            motor_off(channelkipas)
+            time.sleep(60)
+            GPIO.cleanup()
+            motor_on(channellampu)
+            time.sleep(1)
+            motor_off(channellampu)
+            time.sleep(25)
+            GPIO.cleanup()
+            
+            dict_payload["voltage 1"]= data1[0] / 10.0
+            dict_payload["current1_A"] = (data1[1] + (data1[2] << 16)) / 1000.0 # [A]
+            dict_payload["power1_W"] = (data1[3] + (data1[4] << 16)) / 10.0 # [W]
+            dict_payload["energy1_Wh"] = data1[5] + (data1[6] << 16) # [Wh]
+            dict_payload["frequency1_Hz"] = data1[7] / 10.0 # [Hz]
+            dict_payload["power_factor1"] = data1[8] / 100.0
+            dict_payload["alarm1"] = data1[9] # 0 = no alarm
+            str_payload = json.dumps(dict_payload, indent=2)
+
+            dict_payload["voltage2"]= data2[0] / 10.0
+            dict_payload["current2_A"] = (data2[1] + (data2[2] << 16)) / 1000.0 # [A]
+            dict_payload["power2_W"] = (data2[3] + (data2[4] << 16)) / 10.0 # [W]
+            dict_payload["energy2_Wh"] = data2[5] + (data2[6] << 16) # [Wh]
+            dict_payload["frequency2_Hz"] = data2[7] / 10.0 # [Hz]
+            dict_payload["power2_factor"] = data2[8] / 100.0
+            dict_payload["alarm2"] = data2[9] # 0 = no alarm
+            str_payload = json.dumps(dict_payload, indent=2)
+            print(str_payload)
+            time.sleep(2)
+            GPIO.cleanup()
+
+
+    except KeyboardInterrupt:
+        print('exiting pzem script')
+    except Exception as e:
+        print(e)
+    finally:
+        master1.close()
+        master2.close() 
+
